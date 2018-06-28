9f3430788d50440a974f4ea31824ebe8736c0749
HufsaT <htmustafa@gmail.com>
2018-06-27 23:28:02 -0400

    basic classification models uploaded.

--- lambda_test.py
+++ lambda_test.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 some_list = [1,2,3,4]
 some_dict = {1:np.array([[1],[2],[3],[4]])}
@@ -18,5 +20,23 @@ print(opt_choice)
 some_var = opt_choice[0]
 #print(some_var)
 
+somedf = pd.DataFrame({"A": ["cat","dog","snake","cat"],
+                       "B": ["black/white","brown","white","pink"],
+                       "C": [0,1,0,1]})
+#for index, row in somedf.iterrows():
+#    if row["A"] == "cat":
+##        somedf.loc[index,"C"] = 3
+#        print(somedf.loc[[index],["C"]])
+#        somedf["C"].replace(12,3, inplace=True)
+#print(somedf["A"].unique())
 
-#print(something)
+fig = plt.figure(figsize=(10,10))
+i=1
+for a in somedf["A"].unique():
+    fig.add_subplot(3,3,i)
+    plt.title("Animal: {}".format(a))
+    somedf["C"][(somedf.A==a)].value_counts().plot(kind='pie')
+    plt.show()
+    i+=1
+#    plt.plot(somedf["A"][somedf["C"]==0].count(a),(somedf["A"][somedf.C==1].count(a)))
+#    plt.show()
